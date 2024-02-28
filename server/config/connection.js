8b0d8b92dde7660b0348fb3a2616813c386f8dd5
Ben Pavlis <benpavlis@gmail.com>
2024-02-28 14:32:25 -0600

    changed connection file in attempt to fix heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,16 @@
-// const mongoose = require("mongoose");
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/server", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// module.exports = mongoose.connection;
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/madlibsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// module.exports = mongoose.connection;
