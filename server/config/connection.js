e7080dcf08317daac56087b7cb5de9119ab9aa38
Ben Pavlis <benpavlis@gmail.com>
2024-02-28 15:05:00 -0600

    changed connection file in attempt to fix heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/madlibsDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codAppDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
+//-----------
 // const mongoose = require("mongoose");
 
 // mongoose.connect(process.env.MONGODB_URI, {
@@ -14,3 +15,12 @@ module.exports = mongoose.connection;
 // });
 
 // module.exports = mongoose.connection;
+const mongoose = require("mongoose");
+const uri = "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
