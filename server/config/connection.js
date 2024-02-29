e2257702541dc22414456a38aa79ff33e5df7a47
Ben Pavlis <benpavlis@gmail.com>
2024-02-29 16:38:54 -0600

    messed with connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codAppDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codAppDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
 //-----------
 // const mongoose = require("mongoose");
 
@@ -15,12 +15,16 @@
 // });
 
 // module.exports = mongoose.connection;
-const mongoose = require("mongoose");
-const uri = "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//-------
+// const mongoose = require("mongoose");
+// const uri = "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://benpavlis:IcWsM3TEnLTsGMqM@cluster0.evguf1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
+
+
+//mongodb://127.0.0.1:27017/codAppDB
